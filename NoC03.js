fe4dc5429326ccf09d4f27ce1c9472bc67142de2
2z0h0m9 <80618532+2z0h0m9@users.noreply.github.com>
2023-02-03 10:15:54 +0800

    Update NoC03.js

--- NoC03.js
+++ NoC03.js
@@ -3,13 +3,13 @@ var CPacketPlayer = Java.type("net.minecraft.network.play.client.CPacketPlayer")
 var script = registerScript({
     name: "NoC03",
     version: "1.0.0",
-    authors: ["Messenger"]
+    authors: ["Messenger", "2z0h0m9"]
 });
 
 script.registerModule({
     name: "NoC03",
     category: "Misc",
-    description: "取消C03PacketPlayer移动发包"
+    description: "noc03"
 }, function (module) {
 	module.on("enable",
 	function(event) {
